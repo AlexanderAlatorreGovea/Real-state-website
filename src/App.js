c7f1a917596b50b44e9aa05b93aba94aeb789b88
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-08-03 19:01:20 -0700

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import '../src/sass/main.scss';
 import React from 'react';
 import {
   Route,
-  Link,
   BrowserRouter as Router,
-  Switch 
 } from 'react-router-dom';
 import Home from './Home';
 import SignIn from './SignIn';
+import ScrollToTop from './ScrollToTop';
 
 const App = () => {
   return (
     <Router >
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Listings" component={ListingsView} />
-      <Route exact path="/SignIn" component={SignIn} />
+      <ScrollToTop>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Listings" component={ListingsView} />
+        <Route exact path="/SignIn" component={SignIn} />
+      </ScrollToTop>
     </Router>
   );
 };
